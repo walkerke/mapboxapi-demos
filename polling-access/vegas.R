5490096a39c9b96653e6c2d87dbdd1af3cd25e91
Kyle Walker <kwalkerdata@gmail.com>
2020-07-30 10:37:33 -0500

    remove old files

--- polling-access/vegas.R
+++
@@ -1,7 +0,0 @@
-library(mapboxapi)
-library(sf)
-library(leaflet)
-library(fasterize)
-library(esri2sf)
-
-locations <- esri2sf("https://gis.suffolkcountyny.gov/server/rest/services/LocalGovernmentSQLData/PollingPlace/MapServer/0")
